ec5249fd005fa13ee8e20e2ed01f6b830aa1eb93
s220296 <s220296@students.aie.edu.au>
2024-03-04 12:25:01 +1100

    Added horizontal camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,15 +11,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int _health = 1;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _jumpForce = 15f;
+    [SerializeField] private float _lookSensitivity = 1f;
 
     [SerializeField] private InputActionReference _moveAction = null;
     [SerializeField] private InputActionReference _jumpAction = null;
+    [SerializeField] private InputActionReference _lookAction = null;
+    [SerializeField] private InputActionReference _shootAction = null;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
 
         if (_jumpAction) _jumpAction.action.performed += OnJumpPerformed;
+        if (_lookAction) _lookAction.action.performed += OnLookPerformed;
     }
 
     // Update is called once per frame
@@ -28,11 +33,24 @@ public class PlayerController : MonoBehaviour
         OnMovePerformed();
     }
 
+    private void OnLookPerformed(InputAction.CallbackContext cbc)
+    {
+        Vector2 input = cbc.ReadValue<Vector2>();
+        input *= _lookSensitivity;
+
+        Vector3 xRotation = new Vector3(input.y, 0, 0);
+        Vector3 yRotation = new Vector3(0, input.x, 0);
+
+        // Use xRotation to lift & lower camera
+        transform.Rotate(yRotation);
+    }
+
     private void OnMovePerformed()
     {
         Vector2 input = _moveAction.action.ReadValue<Vector2>();
-        input *= _speed;
-        Vector3 movement = new Vector3(input.x, 0, input.y);
+        Vector3 movement = transform.forward * input.y + transform.right * input.x;
+
+        movement *= _speed;
 
         //transform.Translate(movement * Time.deltaTime);
         _rigidbody.MovePosition(transform.position + (movement * Time.deltaTime));
