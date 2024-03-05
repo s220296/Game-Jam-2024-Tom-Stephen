cdc517c3b352464e7a0d7b6e64cfcc8f0a98d39a
s220296 <s220296@students.aie.edu.au>
2024-03-05 16:36:16 +1100

    Added WinBox and medals

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -9,9 +9,40 @@ public class PlayerHUD : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _playerCurrentAmmoDisplay;
     [SerializeField] private TextMeshProUGUI _requiredKillsRemaining;
     [SerializeField] private TextMeshProUGUI _levelTimer;
+    [SerializeField] private TextMeshProUGUI _medalDisplay;
 
     [SerializeField] private string _formatLevelTimer = "F3";
     // Start is called before the first frame update
+    public void SetMedal(Level.Medal medal)
+    {
+        if (!_medalDisplay) return;
+
+        switch(medal)
+        {
+            case Level.Medal.NONE:
+                _medalDisplay.text = "NONE";
+                _medalDisplay.color = Color.gray;
+                break;
+            case Level.Medal.BRONZE:
+                _medalDisplay.text = "BRONZE";
+                _medalDisplay.color = new Color(0.6f, 0.25f, 0, 1);
+                break;
+            case Level.Medal.SILVER:
+                _medalDisplay.text = "SILVER";
+                _medalDisplay.color = new Color(0.8f, 0.8f, 0.8f, 1);
+                break;
+            case Level.Medal.GOLD:
+                _medalDisplay.text = "GOLD";
+                _medalDisplay.color = new Color(1f, 0.9f, 0, 1);
+                break;
+            default:
+                _medalDisplay.text = "";
+                _medalDisplay.color = Color.clear;
+                break;
+
+        }
+    }
+
     public void SetPlayerHealth(int health)
     {
         if(_playerHealthDisplay)
